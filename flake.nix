5f476a79ef4858ab755959730bab74d23c890bc6
MildMax <joseph.s.burns93@gmail.com>
2023-11-02 23:12:23 -0400

    first commit

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "joe b's infra";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Home Manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Secrets
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    darwin,
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    agenix,
+  }: {
+    darwinConfigurations = {
+      jb-mba = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            nixpkgs.config.allowUnfree = true;
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users = {
+                joseph = import ./home/joseph.nix;
+              };
+            };
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home/joseph.nix
@@ -0,0 +1,45 @@
+{
+  pkgs,
+  osConfig,
+  ...
+}: {
+  imports = [
+    applications/zsh.nix
+  ];
+	
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userEmail = ""; #"joesph@protonmail.com";
+    userName = "MildMax";
+    #extraConfig = {
+    #  commit.gpgsign = true;
+    #  user.signingkey = "${osConfig.users.users."joseph".home}/.ssh/id_ed25519.pub";
+    #  gpg = {
+    #    format = "ssh";
+    #    ssh.allowedSignersFile = "${osConfig.users.users."joseph".home}/.ssh/allowed_signers";
+    #  };
+    #};
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "agnoster";
+      plugins = ["git" "docker" "kubectl" "helm"];
+    };
+  };
+
+  home = {
+    username = "joeburns";
+    homeDirectory = "/Users/joeburns";
+    #file."${osConfig.users.users."joseph".home}/.ssh/allowed_signers".text = "* ${builtins.readFile "${osConfig.users.users."joseph".home}/.ssh/id_ed25519.pub"}";
+    packages = with pkgs; [
+      gh
+      sshfs
+    ];
+    stateVersion = "23.05";
+  };
+}
